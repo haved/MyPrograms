aa5a7d6860441d647a4cd326a0a9be8f71bf6373
haved <krogstie.havard@gmail.com>
2015-08-30 16:10:28 +0200

    Chaged the colors a bit :)

--- HDemografiSim/src/MainDemografiSim.cs
+++ HDemografiSim/src/MainDemografiSim.cs
@@ -22,9 +22,9 @@ namespace HDemografiSim
 				Application.Quit();
 				eargs.RetVal = true;
 			};
-			window.ModifyBg (StateType.Normal, new Gdk.Color (100, 255, 140));
+			window.ModifyBg (StateType.Normal, new Gdk.Color (130, 130, 160));
 
-			populationLine = new HStandardChartLine ("Population", new DColor(100, 140, 255));
+			populationLine = new HStandardChartLine ("Population", new DColor(200, 140, 255));
 			populationLine.AddPoint (0, 0);
 			populationLine.AddPoint (1, 2.2f);
 			populationLine.AddPoint (3, 3);
@@ -32,7 +32,7 @@ namespace HDemografiSim
 
 			populationChart = new HChart ("Population chart", populationLine);
 
-			birthRate = new HIndexedChartLine ("Birth rate", new DColor(200, 140, 255));
+			birthRate = new HIndexedChartLine ("Birth rate", new DColor(100, 140, 255));
 			birthRate.AddPoints (1, 1.3f, 1.4f, 1.2f, 1.35f, 2f, 2.6f, 2.7f, 3.2f, 4, 3.6f);
 
 			rateChart = new HChart ("Rates Chart", birthRate);
